318ddfce1011753ce4b877428e4aced17f4c72bb
Jure Čular <jure.cular@gmail.com>
2018-07-13 18:19:27 +0200

    Removed work done for homework 1 and 2

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -10,42 +10,8 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    private var numberOfTaps = 0
-
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var button: UIButton!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.button.layer.cornerRadius = 5.0
-        self.updateLabelWithTapNumber()
-    }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false) { (timer) in
-            self.startOrStopActivityIndicator()
-        }
     }
-
-    @IBAction func didTapButton(_ sender: Any) {
-        print("Button tapped")
-        numberOfTaps += 1
-        self.updateLabelWithTapNumber()
-        self.startOrStopActivityIndicator()
-    }
-
-    private func updateLabelWithTapNumber() {
-        self.label.text = String(self.numberOfTaps)
-    }
-
-    private func startOrStopActivityIndicator() {
-        if (self.activityIndicator.isAnimating) {
-            self.activityIndicator.stopAnimating()
-        } else {
-            self.activityIndicator.startAnimating()
-        }
-    }
-
 }
