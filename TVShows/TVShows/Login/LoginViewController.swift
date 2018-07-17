1e8732d874195a8a316dda1b286fdfd1f8bfd2ce
Jure Čular <jure.cular@gmail.com>
2018-07-17 18:07:40 +0200

    Added rememberMeCheckmark logic

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -11,9 +11,16 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var rememberMeCheckmark: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         logInButton.layer.cornerRadius = 5.0
-
     }
+
+    @IBAction func didPressRememberMeCheckmark(_ sender: Any) {
+        rememberMeCheckmark.isSelected = !rememberMeCheckmark.isSelected
+    }
+
 }
