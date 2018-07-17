e19e4acba76872797dadc09540878b40b4e74402
Jure Čular <jure.cular@gmail.com>
2018-07-17 18:47:16 +0200

    Added keyboard notifications to add inset to scrollView

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var logInButton: UIButton!
     @IBOutlet weak var rememberMeCheckmark: UIButton!
 
@@ -19,7 +20,39 @@ class LoginViewController: UIViewController {
         logInButton.layer.cornerRadius = 5.0
     }
 
-    @IBAction func didPressRememberMeCheckmark(_ sender: Any) {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        registerNotifications()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unregisterNotifications()
+    }
+
+    private func registerNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
+    }
+
+    private func unregisterNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+
+    @objc private func keyboardWillShow(notification: NSNotification){
+        guard
+            let userInfo = notification.userInfo,
+            let keyboardRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+            else { return }
+        scrollView.contentInset.bottom = keyboardRect.size.height
+    }
+
+    @objc private func keyboardWillHide(notification: NSNotification){
+        scrollView.contentInset.bottom = 0
+    }
+
+    @IBAction private func didPressRememberMeCheckmark(_ sender: Any) {
         rememberMeCheckmark.isSelected = !rememberMeCheckmark.isSelected
     }
 
