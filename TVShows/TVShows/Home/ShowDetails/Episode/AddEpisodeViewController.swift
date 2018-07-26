07c03a2c1039fd93e409c05d75ac4bc19660ec3a
Jure Čular <jure.cular@gmail.com>
2018-07-26 14:18:19 +0200

    Added events to hide/show keyboard

--- TVShows/TVShows/Home/ShowDetails/Episode/AddEpisodeViewController.swift
+++ TVShows/TVShows/Home/ShowDetails/Episode/AddEpisodeViewController.swift
@@ -20,6 +20,16 @@ class AddEpisodeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        _registerNotifications()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        _unregisterNotifications()
+    }
     }
 
     }
@@ -44,6 +54,40 @@ class AddEpisodeViewController: UIViewController {
     @IBAction private func _didTapUploadPhoto(_ sender: Any) {
     }
 
+    // MARK: - Notifications -
+
+    private func _registerNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(_keyboardWillShow),
+                                               name: .UIKeyboardWillShow,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(_keyboardWillHide),
+                                               name: .UIKeyboardWillHide,
+                                               object: nil)
+    }
+
+    private func _unregisterNotifications() {
+        NotificationCenter.default.removeObserver(self,
+                                                  name: .UIKeyboardWillShow,
+                                                  object: nil)
+        NotificationCenter.default.removeObserver(self,
+                                                  name: .UIKeyboardWillHide,
+                                                  object: nil)
+    }
+
+    @objc private func _keyboardWillShow(notification: NSNotification){
+        guard
+            let userInfo = notification.userInfo,
+            let keyboardRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+            else { return }
+        _scrollView.contentInset.bottom = keyboardRect.size.height
+        _scrollView.scrollIndicatorInsets.bottom = keyboardRect.size.height
+    }
+
+    @objc private func _keyboardWillHide(notification: NSNotification){
+        _scrollView.contentInset.bottom = 0
+        _scrollView.scrollIndicatorInsets.bottom = 0
     }
 
 }
