4801bba1ef6efda2bd50b71a07a7501cbeab26f2
Jure Čular <jure.cular@gmail.com>
2018-07-26 11:51:00 +0200

    Added AddEpisodeViewController

---
+++ TVShows/TVShows/Home/ShowDetails/Episode/AddEpisodeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddEpisodeViewController.swift
+//  TVShows
+//
+//  Created by Jure Cular on 26/07/2018.
+//  Copyright © 2018 Jure Čular. All rights reserved.
+//
+
+import UIKit
+
+class AddEpisodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
