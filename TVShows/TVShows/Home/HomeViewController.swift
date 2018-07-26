baef33f4c2218c4c1931ee28924341932640877d
Jure Čular <jure.cular@gmail.com>
2018-07-26 11:15:36 +0200

    Added present ShowDetailsViewController on show tap on HomeViewController

--- TVShows/TVShows/Home/HomeViewController.swift
+++ TVShows/TVShows/Home/HomeViewController.swift
@@ -72,6 +72,13 @@ extension HomeViewController: Progressable, Alertable {
 
 extension HomeViewController: UITableViewDelegate {
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let show = _shows[indexPath.row]
+
+        let showDetailsViewController = ShowDetailsViewController.initFromStoryboard(withToken: _loginUser.token, showID: show.id)
+        navigationController?.show(showDetailsViewController, sender: self)
+    }
+
 }
 
 // MARK: - UITableViewDataSource -
