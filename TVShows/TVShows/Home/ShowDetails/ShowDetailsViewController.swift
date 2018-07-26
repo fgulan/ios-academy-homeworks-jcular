bc8cdfdf7c4551705511f5e6963698643b986fb6
Jure Čular <jure.cular@gmail.com>
2018-07-26 11:14:24 +0200

    Added table view delegate and table view datasource for ShowDetailsViewController

--- TVShows/TVShows/Home/ShowDetails/ShowDetailsViewController.swift
+++ TVShows/TVShows/Home/ShowDetails/ShowDetailsViewController.swift
@@ -94,3 +94,72 @@ extension ShowDetailsViewController: Progressable, Alertable {
     }
 
 }
+
+// MARK: - UITableViewDelegate -
+
+extension ShowDetailsViewController: UITableViewDelegate {
+
+}
+
+// MARK: - UITableViewDataSource -
+
+extension ShowDetailsViewController: UITableViewDataSource {
+
+    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete && indexPath.row > 2 {
+            _episodes.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+
+    public func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        switch indexPath.row {
+        case 0, 1:
+            return .none
+        default:
+            return .delete
+        }
+    }
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return _episodes.count + 2
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        switch indexPath.row {
+            case 0:
+                let cell = tableView.dequeueReusableCell(
+                    withIdentifier: "ShowImageTableViewCell",
+                    for: indexPath
+                    ) as! ShowImageTableViewCell
+                // TODO: remove Image placeholder, use image url from ShowDetails
+                if let image = UIImage(named: "img-placeholder-user1") {
+                    cell.configure(image: image)
+                }
+
+                cell.selectionStyle = .none
+                return cell
+            case 1:
+                let cell = tableView.dequeueReusableCell(
+                    withIdentifier: "ShowDescriptionTableViewCell",
+                    for: indexPath
+                ) as! ShowDescriptionTableViewCell
+                if let showDetails = _showDetails {
+                    cell.configure(showDetails: showDetails, episodesNumber: _episodes.count)
+                }
+
+                cell.selectionStyle = .none
+                return cell
+            default:
+                let cell = tableView.dequeueReusableCell(
+                    withIdentifier: "EpisodeTableViewCell",
+                    for: indexPath
+                ) as! EpisodeTableViewCell
+                cell.configure(episode: _episodes[indexPath.row - 2])
+
+                return cell
+        }
+    }
+
+}
