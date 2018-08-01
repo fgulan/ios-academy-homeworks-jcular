5ab058122f7cefec3fed3c37b61a6f5f7e0dba8f
Jure Čular <jure.cular@gmail.com>
2018-08-01 23:00:43 +0200

    Added CommentsViewController and Storyboard

---
+++ TVShows/TVShows/Modules/Comments/CommentsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CommentsViewController.swift
+//  TVShows
+//
+//  Created by Jure Cular on 01/08/2018.
+//  Copyright © 2018 Jure Čular. All rights reserved.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+}
